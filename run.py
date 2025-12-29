29eea3921c4ec4698735d2271f94a1609b3260cf
manass2268 <manassingh1509@gmail.com>
2025-12-29 12:06:02 +0530

    Your descriptive commit message

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ def listenHotword():
     print("Process 2" \
     " Listening for hotword...")
     from Backend.feature import hotword
-    hotword()
+    hotword()   
 if __name__ == "__main__":
     process1= multiprocessing.Process(target=startJarvis)
     process2= multiprocessing.Process(target=listenHotword)
